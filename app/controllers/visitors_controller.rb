c4d4d89f47fa9f7ba754e68002b205ef2e42b1e2
Youssef Altherwy <ysf200@gmail.com>
2017-02-23 14:44:44 +0300

    Front-End Framework

--- app/controllers/visitors_controller.rb
+++ app/controllers/visitors_controller.rb
@@ -1,6 +1,6 @@
 class VisitorsController < ApplicationController
     def new 
         @owner = Owner.new
-        flash.now[:notice] = 'Hi'
+        #flash.now[:notice] = 'Hi'  (ALERT MESSAGE)
     end
 end
